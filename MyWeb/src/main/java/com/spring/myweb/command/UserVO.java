cca4aea85097b6b9c301fbd01d09183920251492
Jane <ribbon0628@naver.com>
2023-05-17 18:43:43 +0900

    23.05.17

---
+++ MyWeb/src/main/java/com/spring/myweb/command/UserVO.java
@@ -0,0 +1,30 @@
+package com.spring.myweb.command;
+
+import java.time.LocalDateTime;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/*
+ 	CREATE TABLE users (
+	    user_id VARCHAR(50) PRIMARY KEY,
+	    user_pw VARCHAR(50) NOT NULL,
+	    user_name VARCHAR(50) NOT NULL,
+	    user_phone1 VARCHAR(50),
+	    user_phone2 VARCHAR(50),
+	    user_email1 VARCHAR(50),
+	    user_email2 VARCHAR(50),
+	    addr_basic VARCHAR(300),
+	    addr_detail VARCHAR(300),
+	    addr_zip_num VARCHAR(50),
+	    reg_date DATETIME DEFAULT CURRENT_TIMESTAMP
+	);
+ */
+
+@Getter @Setter @ToString
+public class UserVO {
+	
+	private String userId, userPw, userName, userPhone1, userPhone2, userEmail1, userEmail2, addrBasic, addrDetail, addrZipNum;
+	private LocalDateTime regDate;
+}
